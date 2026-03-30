dd043901edc8aa1363e3cb9e01969bc34a65fe47
William-SWS <samuelwill.ds@gmail.com>
2026-03-30 15:42:51 -0300

    primeiro commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main 
+
+import(
+	"fmt"
+	"time"
+) 
+
+func main() {
+	fmt.Println("Hello world")
+
+	fmt.Println("A hora de agora é " ,time.Now())
+}
---
+++ math.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+	"fmt"
+	"math/rand"
+	"math"
+)
+
+func main() {
+	fmt.Println("meu numero favorito é ", rand.Intn(10))
+
+	fmt.Println("Agora nos temos %g problemas. \n", math.Sqrt(7))
+}
